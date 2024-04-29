e9244f8a3c6950b26483e56b71514ee1d6d1ae89
José Alves Feitosa Neto <netoofeitosa@gmail.com>
2024-04-29 10:34:32 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/app_dmetal_dev/sw.js', { scope: '/app_dmetal_dev/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
