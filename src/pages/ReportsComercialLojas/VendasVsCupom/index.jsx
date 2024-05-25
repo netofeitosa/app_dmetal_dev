14d99c875cd750db4da771c57bb7268664e5f2a8
José Alves Feitosa Neto <netoofeitosa@gmail.com>
2024-05-25 14:58:19 -0300

    Layout pagina Reports Vendas x Cupom

--- src/pages/ReportsComercialLojas/VendasVsCupom/index.jsx
+++ src/pages/ReportsComercialLojas/VendasVsCupom/index.jsx
@@ -2,7 +2,15 @@ import React, { useEffect, useState } from "react";
 import { Api } from "../../../services/api";
 import Spinner from "../../../components/Spinner";
 import ChartVendasVsCupom from "../../../components/Charts/ChartVendasVsCupom";
-import { Chart, Resume, Section } from "./vendasvscupom.style";
+import {
+  Chart,
+  Resume,
+  ResumeCol1,
+  ResumeCol2,
+  ResumeDivider,
+  Section,
+} from "./vendasvscupom.style";
+import { Divider } from "antd";
 
 const VendasVsCupom = () => {
   const [dados, setDados] = useState();
@@ -26,9 +34,41 @@ const VendasVsCupom = () => {
   ) : (
     <Section>
       <Resume>
-        <p>Total Venda: {dados.Resultado[0].total_venda_format}</p>
-        <p>Total Cupom: {dados.Resultado[0].total_cupom_format}</p>
-        <p>Perc: {dados.Resultado[0].perc}</p>
+        <ResumeCol1>
+          <div>
+            <span>Mês Atual</span>
+          </div>
+          <Divider />
+          <div>
+            <span>Total Venda </span>
+            <span>{dados.Resultado[0].total_venda_format}</span>
+          </div>
+          <div>
+            <span>Total Cupom</span>
+            <span>{dados.Resultado[0].total_cupom_format}</span>
+          </div>
+          <div>
+            <span>{dados.Resultado[0].perc.toFixed(2)}%</span>
+          </div>
+        </ResumeCol1>
+
+        <ResumeCol2>
+          <div>
+            <span>Mês Anterior</span>
+          </div>
+          <Divider />
+          <div>
+            <span>Total Venda </span>
+            <span>{dados.Resultado[0].total_venda_ant_format}</span>
+          </div>
+          <div>
+            <span>Total Cupom</span>
+            <span>{dados.Resultado[0].total_cupom_ant_format}</span>
+          </div>
+          <div>
+            <span>{dados.Resultado[0].perc_ant.toFixed(2)}%</span>
+          </div>
+        </ResumeCol2>
       </Resume>
       <Chart>
         <ChartVendasVsCupom dados={dados} />
--- src/pages/ReportsComercialLojas/VendasVsCupom/vendasvscupom.style.jsx
+++ src/pages/ReportsComercialLojas/VendasVsCupom/vendasvscupom.style.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { theme } from "../../../Theme";
 
 export const Section = styled.section`
   display: flex;
@@ -11,13 +12,86 @@ export const Section = styled.section`
 
 export const Resume = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: start;
+  //flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
   border-radius: 8px;
   background-color: #ffffff;
   width: 90%;
-  padding: 10px;
-  gap: 5px;
+  padding: 15px;
+  gap: 10px;
+  font-size: 14px;
+`;
+
+export const ResumeCol1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 8px;
+
+  & div {
+    display: flex;
+    flex-direction: column;
+  }
+
+  & div:nth-child(1) {
+    width: 100%;
+    font-size: 16px;
+    font-weight: bold;
+    align-items: center;
+  }
+
+  & span:nth-child(1) {
+    font-weight: bold;
+  }
+
+  & div:nth-child(5) {
+    font-size: 18px;
+    background-color: ${({ Theme }) => theme.color.background};
+    width: 90%;
+    padding: 10px 0px;
+    align-items: center;
+    border-radius: 6px;
+  }
+`;
+
+export const ResumeCol2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: end;
+  //padding: 5px;
+  gap: 8px;
+
+  & div {
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+  }
+
+  & div:nth-child(1) {
+    width: 100%;
+    font-size: 16px;
+    font-weight: bold;
+    align-items: center;
+  }
+
+  & span:nth-child(1) {
+    font-weight: bold;
+  }
+
+  & div:nth-child(5) {
+    font-size: 18px;
+    background-color: ${({ Theme }) => theme.color.background};
+    width: 90%;
+    padding: 10px 0px;
+    align-items: center;
+    border-radius: 6px;
+  }
+`;
+
+export const ResumeDivider = styled.div`
+  margin-bottom: 5px;
 `;
 
 export const Chart = styled.div`
